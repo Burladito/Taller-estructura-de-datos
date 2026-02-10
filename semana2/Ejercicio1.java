302fd5d5666c3294e7da9eaadb9189c2f173c2b7
Burladito <sacunam@estudiante.uniajc.edu.co>
2026-02-10 16:46:38 +0000

    se agrego sumar y mostrar pares y impares

--- semana2/Ejercicio1.java
+++ semana2/Ejercicio1.java
@@ -8,5 +8,16 @@ public class Ejercicio1 {
         for (int i = 0; i < a.length; i++) {
             System.out.println("a[" + i + "]=" + a[i]);
         }
+        int sumaimpar = 0;
+        int sumapar = 0;  
+        for (int i = 0; i < a.length; i++) {
+            if (a[i] % 2 == 0) {
+                sumapar += a[i];
+            } else {
+                sumaimpar += a[i];
+            }
+        }
+        System.out.println("Suma de pares: " + sumapar);
+        System.out.println("Suma de impares: " + sumaimpar);
     }
 }
