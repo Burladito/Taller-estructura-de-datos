c5fcee5d373b3fc823be1146b22494aeaba121b9
Burladito <sacunam@estudiante.uniajc.edu.co>
2026-02-17 16:29:09 +0000

    se agrego setters and getters

---
+++ ArreglosdeObjetos/EjecutarEstudiante.java
@@ -0,0 +1,8 @@
+package ArreglosdeObjetos;
+
+public class EjecutarEstudiante {
+    public static void main(String[] args) {
+    // Arreglo de Objetos
+    Estudiante[] estudiantes = new Estudiante[5];
+}
+}
