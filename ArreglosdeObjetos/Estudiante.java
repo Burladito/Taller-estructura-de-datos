b043553b6d622b6d39142cafcefecf956a6e5d33
Burladito <sacunam@estudiante.uniajc.edu.co>
2026-02-17 15:39:39 +0000

    se agrego los private

---
+++ ArreglosdeObjetos/Estudiante.java
@@ -0,0 +1,9 @@
+package ArreglosdeObjetos;
+
+public class Estudiante {
+    private String nombre;
+    private int semestre;
+    private String programa;
+    private double notaPromedio;
+    private int edad;
+}
