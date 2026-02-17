7a1dc168ec395659a2ea3bc7747b854524a7a5f0
Burladito <sacunam@estudiante.uniajc.edu.co>
2026-02-17 16:56:40 +0000

    double SumaNotas

--- ArreglosdeObjetos/EjecutarEstudiante.java
+++ ArreglosdeObjetos/EjecutarEstudiante.java
@@ -10,5 +10,14 @@ public class EjecutarEstudiante {
         est[3] = new Estudiante("Milena", 3, "Ingeniería de Sistemas", 3.7, 20);
         
         System.out.println(est[2]);
+        for (int i = 0; i < est.length; i++) {
+            System.out.println(est[i].getNombre()+" --> "+est[i].getPrograma());
+        }
+
+        double sumaNotas = 0.0;
+        for (int i = 0; i < est.length; i++) {
+            sumaNotas += est[i].getNotaPromedio();
+        }
+        System.out.println(sumaNotas/est.length);
     }
 }
