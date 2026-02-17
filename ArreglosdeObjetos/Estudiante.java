66baa3ba26306c955da0f6952bee99b37d25929c
Burladito <sacunam@estudiante.uniajc.edu.co>
2026-02-17 15:40:35 +0000

    Se Creo el Constructor

--- ArreglosdeObjetos/Estudiante.java
+++ ArreglosdeObjetos/Estudiante.java
@@ -6,4 +6,11 @@ public class Estudiante {
     private String programa;
     private double notaPromedio;
     private int edad;
+    // constructor
+    public Estudiante(String nombre, int semestre, String programa, double notaPromedio, int edad) {
+        this.nombre = nombre;
+        this.semestre = semestre;
+        this.programa = programa;
+        this.notaPromedio = notaPromedio;
+        this.edad = edad;
 }
