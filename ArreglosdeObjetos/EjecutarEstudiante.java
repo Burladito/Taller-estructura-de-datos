7bfcb8b0fb6472bdf4e1df319f475b81ad0dee21
Burladito <sacunam@estudiante.uniajc.edu.co>
2026-02-24 14:13:18 +0000

    se agrego metodo que pidio el profe

--- ArreglosdeObjetos/EjecutarEstudiante.java
+++ ArreglosdeObjetos/EjecutarEstudiante.java
@@ -6,17 +6,23 @@ public class EjecutarEstudiante {
         Estudiante objE = new Estudiante();
  
         //Arreglo de Objetos
-        Estudiante[] est = new Estudiante[4];
+        Estudiante[] est = new Estudiante[10];
  
         est[0] = new Estudiante("Juan", 3, "Ingeniería de Sistemas", 3.8, 20);
         est[1] = new Estudiante("Miguel", 4, "Ingeniería de Sistemas", 4.2, 19);
         est[2] = new Estudiante("Armando", 3, "Ingeniería de Sistemas", 3.0, 21);
         est[3] = new Estudiante("Milena", 3, "Ingeniería de Sistemas", 4.5, 20);
-       
+        est[4] = new Estudiante("Sofia", 4, "Ingeniería de Sistemas", 3.9, 19);
+        est[5] = new Estudiante("Camila", 3, "Ingeniería de Sistemas", 4.0, 20);
+        est[6] = new Estudiante("Valentina", 4, "Ingeniería de Sistemas", 3.7, 21);
+        est[7] = new Estudiante("Andres", 3, "Ingeniería de Sistemas", 4.1, 20);
+        est[8] = new Estudiante("Santiago", 4, "Ingeniería de Sistemas", 3.6, 19);
+        est[9] = new Estudiante("Camilo", 3, "Ingeniería de Sistemas", 4.3, 20);    
+        
         System.out.println(est[2]);
  
         for (int i = 0; i < est.length; i++) {
-            System.out.println(est[i].getNombre() + " --> " + est[i].getPrograma());        
+            System.out.println(est[i].getNombre() + " --> " + est[i].getPrograma() + " --> " + est[i].getNotaPromedio());        
         }
  
         //Promedio general de las notas de los estudiantes
@@ -34,7 +40,7 @@ public class EjecutarEstudiante {
  
         //Uso del método calcularEdadPromedio
         System.out.println(objE.calcularEdadPromedio(edades));
- 
+        
        
     }    
 }
