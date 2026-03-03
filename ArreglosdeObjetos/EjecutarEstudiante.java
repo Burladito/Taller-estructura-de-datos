674cf46bc9870dcf6666b4eaf9e23df3839b714b
Burladito <sacunam@estudiante.uniajc.edu.co>
2026-03-03 17:19:28 +0000

    cambio

--- ArreglosdeObjetos/EjecutarEstudiante.java
+++ ArreglosdeObjetos/EjecutarEstudiante.java
@@ -2,12 +2,12 @@ package ArreglosdeObjetos;
 
 public class EjecutarEstudiante {
     public static void main(String[] args) {
-        //Creación de un objeto de la clase estudiante
+        // Creación de un objeto de la clase estudiante
         Estudiante objE = new Estudiante();
- 
-        //Arreglo de Objetos
+
+        // Arreglo de Objetos
         Estudiante[] est = new Estudiante[10];
- 
+
         est[0] = new Estudiante("Juan", 3, "Ingeniería Mecatronica", 3.8, 20);
         est[1] = new Estudiante("Miguel", 4, "Ingeniería Industrial", 4.2, 19);
         est[2] = new Estudiante("Armando", 3, "Ingeniería de Software", 3.0, 21);
@@ -17,39 +17,40 @@ public class EjecutarEstudiante {
         est[6] = new Estudiante("Valentina", 4, "Ingeniería de Sistemas", 3.7, 21);
         est[7] = new Estudiante("Andres", 3, "Ingeniería de Sistemas", 4.1, 20);
         est[8] = new Estudiante("Santiago", 4, "Ingeniería de Sistemas", 3.6, 19);
-        est[9] = new Estudiante("Camilo", 3, "Ingeniería de Sistemas", 4.3, 20);    
-        
+        est[9] = new Estudiante("Camilo", 3, "Ingeniería de Sistemas", 4.3, 20);
+
         System.out.println(est[2]);
- 
+
         for (int i = 0; i < est.length; i++) {
-            System.out.println(est[i].getNombre() + " --> " + est[i].getPrograma() + " --> " + est[i].getNotaPromedio());        
+            System.out
+                    .println(est[i].getNombre() + " --> " + est[i].getPrograma() + " --> " + est[i].getNotaPromedio());
         }
- 
-        //Promedio general de las notas de los estudiantes
-        double sumaNota = 0.0; 
+
+        // Promedio general de las notas de los estudiantes
+        double sumaNota = 0.0;
         for (int i = 0; i < est.length; i++) {
-            sumaNota += est[i].getNotaPromedio();        
+            sumaNota += est[i].getNotaPromedio();
         }
         System.out.println(sumaNota / est.length);
- 
-        //Almacenar en el arreglo edades la edad de los estudiantes
+
+        // Almacenar en el arreglo edades la edad de los estudiantes
         int[] edades = new int[est.length];
         for (int i = 0; i < edades.length; i++) {
             edades[i] = est[i].getEdad();
         }
- 
-        //Uso del método calcularEdadPromedio
+
+        // Uso del método calcularEdadPromedio
         System.out.println(objE.calcularEdadPromedio(edades));
-        
+
         int semestreBuscado = 4;
 
-System.out.println("Estudiantes del semestre " + semestreBuscado + ":");
+        System.out.println("Estudiantes del semestre " + semestreBuscado + ":");
 
-for (int i = 0; i < est.length; i++) {
-    if (est[i].getSemestre() == semestreBuscado) {
-        System.out.println("Nombre: " + est[i].getNombre() +
-                           " --> Carrera: " + est[i].getPrograma());
+        for (int i = 0; i < est.length; i++) {
+            if (est[i].getSemestre() == semestreBuscado) {
+                System.out.println("Nombre: " + est[i].getNombre() +
+                        " --> Carrera: " + est[i].getPrograma());
+            }
+        }
     }
-}    
-}
 }
